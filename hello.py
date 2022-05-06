25efa6659d78507511d054bbc6e69350849c900f
PauloCesar-Souza <program.desevelop@protonmail.com>
2022-05-06 09:04:02 -0300

    Adiconado SHEBANG e um comentário

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-#! -> SHEBANG
+ # #!C:\projects\Curso_Python_BrunoRocha\PyhtonBaseCurso\hello.py  -> SHEBANG
 
 # Este programa imprime Hello world
 
 print("Hello, Word!")  
-print("Hello, Word!") 
