07ce037302718764ed6859dc83eb9d1344b0a42c
PauloCesar-Souza <program.desevelop@protonmail.com>
2022-06-19 21:42:42 -0300

    Curso Base Python Programas Day2

---
+++ escola_v2_com_sets.py
@@ -0,0 +1,43 @@
+# #! /usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por salas que frequentas cada uma das 
+atividades.
+"""
+__Version__ = "0.1.0"
+
+sala1 = ["Erick", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erick", "Maia", "Joana", "Antonio", "Carlos"]
+aula_musica = ["Erick", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [
+    ("Inglês", aula_ingles),
+    ("Música", aula_musica),
+    ("Dança", aula_danca),
+]
+
+# Listar alunis em cada atividade por sala
+
+for nome_atividade, atividade in atividades:
+
+    print(f"Alunos da atividade {nome_atividade}\n")
+    print("-" * 40)
+
+    atividade_sala1 = []
+    atividade_sala2 = []
+
+    for aluno in atividade:
+    
+        if aluno in sala1:
+            atividade_sala1.append(aluno)
+        elif aluno in sala2:
+            atividade_sala2.append(aluno) 
+
+    print(f"Sala1", atividade_sala1)
+    print(f"Sala2", atividade_sala2)
+
+    print()
+    print("#" * 40)
---
+++ tabuada.py
@@ -0,0 +1,39 @@
+#  #!/usr/bin/env python
+"""Imprime tabuada do 0 ao 10 
+
+---Tabuada do 1---
+
+1 X 1 = 1 
+2 X 1 = 2
+3 X 1 = 3
+....
+########################
+-------------
+---Tabuada do 2---
+
+2 X 1 = 1
+2 X 2 = 4
+....
+########################
+"""
+__version__ = "0.1.1"
+__author__ = "Paulo Cesar"
+
+
+#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#Iterable (percorriveis)
+numeros = list(range(1, 11))
+
+#para cada numero em numeros:
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print()     
+    print("#" * 18)
+    print()
+
+
+
