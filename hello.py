80f65595c0fb7c8f71ef4945843e0c30983faf9e
PauloCesar-Souza <program.desevelop@protonmail.com>
2022-07-04 17:09:32 -0300

    Novo

--- hello.py
+++ hello.py
@@ -1,17 +1,11 @@
- # #!C:\projects\Curso_Python_BrunoRocha\PyhtonBaseCurso\hello.py  -> SHEBANG
+# #!C:\projects\Curso_Python_BrunoRocha\PyhtonBaseCurso\hello.py  -> SHEBANG
 """Hello World Multi Linguas
-
 Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
 correspondente.
-
 Como usar:
-
 Tenha a variável LANG devidamente configurada ex:
-
     export LANG=pt_BR
-
 Execução:
-
     python3 hello.py
     ou
     ./hello.py
